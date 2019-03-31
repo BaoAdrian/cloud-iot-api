848825839eb231ff5ace65b026d0a3e350ee9bc8
EC2 Default User <ec2-user@ip-172-31-38-154.ec2.internal>
2019-03-31 08:05:44 +0000

    Add databse query functionality to color table

--- dev/color-table.py
+++ dev/color-table.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 
+import MySQLdb
 import random
 import cgitb
+import cgi
 cgitb.enable()
-print("Content-Type: text/html;charset=utf-8")
 
+# Create HTML Environment and print header
+print("Content-Type: text/html;charset=utf-8")
 print("Content-type:text/html\r\n\r\n")
 print('<html>')
 print('<head>')
 print('<title>Color Table</title>')
 print('</head>')
+
+# Setup Connection to Database
+conn = MySQLdb.connect( host 	= 'project2-iot-lightbulb.ccdjm6mfovyw.us-west-1.rds.amazonaws.com',
+			user 	= 'Admin',
+			passwd 	= 'project2password',
+			db 	= 'project2db')
+
+# Setup cursor to query database
+cursor = conn.cursor()
+
+# Set up form
+form = cgi.FieldStorage()
 
 # Generate RGB for random color
 rand_red = random.randint(0, 255)
@@ -29,20 +44,56 @@ turq_config = [0,206,209]
 maroon_config = [128,0,0]
 random_config = [rand_red, rand_green, rand_blue]
 
+# Analyze the form and update database
+r_conf = form["red"].value
+g_conf = form["green"].value
+b_conf = form["blue"].value
+opacity = form["opacity"].value
 
+print("""<h1> %s </h1>
+	<h1> %s </h1>
+	<h1> %s </h1>""" % (r_conf, g_conf, b_conf))
+
+#cursor.execute("""UPDATE light_status SET red=%s, green=%s, blue=%s, opacity=%s;""",(r_conf, g_conf, b_conf, opacity))
+
+cursor.execute("""UPDATE light_status SET red=%s, green=%s, blue=%s, opacity=%s;""",(r_conf, g_conf, b_conf, opacity))
+
+# Print lightbulb
 print("""<body>
-		<br/>
+	<center>
+                <h1 style="color:rgb(%d,%d,%d)">""" % (int(r_conf), int(g_conf), int(b_conf)))
+          
+print("""<svg align="center" viewBox="0 0 60 55" preserveAspectRatio="xMidYMin slice"
+   style="width: 30%; padding-bottom: 30%; height: 1px; overflow: visible">
+  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="lightbulb" class="svg-inline--fa fa-lightbulb fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill-opacity="0.9" fill="currentColor" d="M96.06 454.35c.01 6.29 1.87 12.45 5.36 17.69l17.09 25.69a31.99 31.99 0 0 0 26.64 14.28h61.71a31.99 31.99 0 0 0 26.64-14.28l17.09-25.69a31.989 31.989 0 0 0 5.36-17.69l.04-38.35H96.01l.05 38.35zM0 176c0 44.37 16.45 84.85 43.56 115.78 16.52 18.85 42.36 58.23 52.21 91.45.04.26.07.52.11.78h160.24c.04-.26.07-.51.11-.78 9.85-33.22 35.69-72.6 52.21-91.45C335.55 260.85 352 220.37 352 176 352 78.61 272.91-.3 175.45 0 73.44.31 0 82.97 0 176zm176-80c-44.11 0-80 35.89-80 80 0 8.84-7.16 16-16 16s-16-7.16-16-16c0-61.76 50.24-112 112-112 8.84 0 16 7.16 16 16s-7.16 16-16 16z"></path></svg>
+  </svg>
+</svg>
+<h1/>
+<center/>""")
+
+
+# Print Page Header
+print("""<br/>
 		<h1 align="center"> Cloud Computing - Project 2 - IoT Web Application</h1>
 		<h2 align="center"> Color Table </h2>
 		<br/>
 
-		<p align="center">
-		<button type="button" align="left"   style="height:200px; width:200px; background:rgb(255,0,0); font-size:24px;">Red</button>
-		<button type="button" align="center" style="height:200px; width:200px; background:rgb(0,255,0); font-size:24px;">Green</button>
-		<button type="button" align="right"  style="height:200px; width:200px; background:rgb(0,0,255); font-size:24px;">Blue</button>
-		</p>
+		<p align="center">""")
 
-		<p align="center">
+#Setup all buttons with their corresponding HTTP URLs
+
+# Red
+print("""<button type="button" onclick="window.location.href = 'http://ec2-107-23-175-18.compute-1.amazonaws.com/cgi-bin/color-table.py?red=%s&green=%s&blue=%s&opacity=%s'" align="left"   style="height:200px; width:200px; background:rgb(255,0,0); font-size:24px;">Red</button>""" % (red_config[0], red_config[1], red_config[2], opacity))
+
+# Green
+print("""<button type="button" onclick="window.location.href = 'http://ec2-107-23-175-18.compute-1.amazonaws.com/cgi-bin/color-table.py?red=%s&green=%s&blue=%s&opacity=%s'" align="center" style="height:200px; width:200px; background:rgb(0,255,0); font-size:24px;">Green</button>""" % (green_config[0], green_config[1], green_config[2], opacity))
+
+# Blue
+print("""<button type="button" onclick="window.location.href = 'http://ec2-107-23-175-18.compute-1.amazonaws.com/cgi-bin/color-table.py?red=%s&green=%s&blue=%s&opacity=%s'" align="right"  style="height:200px; width:200px; background:rgb(0,0,255); font-size:24px;">Blue</button>
+		</p>""" % (blue_config[0], blue_config[1], blue_config[2], opacity))
+
+
+print("""<p align="center">
                 <button type="button" align="left"   style="height:200px; width:200px; background:rgb(255,127,80);  font-size:24px;">Orange</button>
                 <button type="button" align="center" style="height:200px; width:200px; background:rgb(147,112,219); font-size:24px;">Purple</button>
                 <button type="button" align="right"  style="height:200px; width:200px; background:rgb(255,255,0);   font-size:24px;">Yellow</button>
@@ -67,4 +118,12 @@ print("""<body>
 	</body>""" % (rand_red, rand_green, rand_blue, rand_red, rand_green, rand_blue))
 
 print('</html>')
+
+
+# Commit changes to Datbase and Cleanup
+conn.commit()
+cursor.close()
+conn.close()
+
+
 
