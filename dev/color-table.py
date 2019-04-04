feb149a4144b74bb5e9bd03e6284b3c76214b3f6
root <root@ip-172-31-91-145.ec2.internal>
2019-04-04 02:51:44 +0000

    :)

--- dev/color-table.py
+++ dev/color-table.py
@@ -70,9 +70,10 @@ random_config = [rand_red, rand_green, rand_blue]
 # Print lightbulb
 print("""<body>
                 <h1 align="center"> Cloud Computing - Project 2 - IoT Web Application</h1>
+<button type="button" onclick="window.location.href = 'http://%s/cgi-bin/welcome.py';">Home</button>
                 <h2 align="center"> Color Table </h2>
 	<center>
-                <h1 style="color:rgb(%d,%d,%d)">""" % (int(r_conf), int(g_conf), int(b_conf)))
+                <h1 style="color:rgb(%d,%d,%d)">""" % (ip.IP_ADDR, int(r_conf), int(g_conf), int(b_conf)))
           
 print("""<svg align="center" viewBox="0 0 15 10" preserveAspectRatio="xMidYMin slice" style="width: 30%; padding-bottom: 20%; height: 1px; overflow: visible">
   	<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="lightbulb" class="svg-inline--fa fa-lightbulb fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill-opacity="0.9" fill="currentColor" d="M96.06 454.35c.01 6.29 1.87 12.45 5.36 17.69l17.09 25.69a31.99 31.99 0 0 0 26.64 14.28h61.71a31.99 31.99 0 0 0 26.64-14.28l17.09-25.69a31.989 31.989 0 0 0 5.36-17.69l.04-38.35H96.01l.05 38.35zM0 176c0 44.37 16.45 84.85 43.56 115.78 16.52 18.85 42.36 58.23 52.21 91.45.04.26.07.52.11.78h160.24c.04-.26.07-.51.11-.78 9.85-33.22 35.69-72.6 52.21-91.45C335.55 260.85 352 220.37 352 176 352 78.61 272.91-.3 175.45 0 73.44.31 0 82.97 0 176zm176-80c-44.11 0-80 35.89-80 80 0 8.84-7.16 16-16 16s-16-7.16-16-16c0-61.76 50.24-112 112-112 8.84 0 16 7.16 16 16s-7.16 16-16 16z"></path></svg>
