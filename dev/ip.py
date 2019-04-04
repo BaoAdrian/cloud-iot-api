79996df9818b0b656b35ccd81dd8c38a2996f4d2
EC2 Default User <ec2-user@ip-172-31-38-154.ec2.internal>
2019-04-04 01:56:06 +0000

    change IP

--- dev/ip.py
+++ dev/ip.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 # Replace with Public DNS (IPv4) for EC2 Instance
-IP_ADDR = "ec2-54-198-0-3.compute-1.amazonaws.com"
+IP_ADDR = "ec2-34-238-167-13.compute-1.amazonaws.com"
